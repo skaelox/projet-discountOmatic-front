14feaf09fc052f511af001a0aec774710ed97237
cemerlin80 <cemerlin@outlook.fr>
2021-02-15 10:40:14 +0100

    Ajout de Home

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Home from './page/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* Navbar ici */}
+
+{/* Ajouter les liens ici */}
+<Switch>
+
+  {/* Ajouter les routes ici */}
+  <Route exact path="/">
+    <Home/>
+  </Route>
+
+</Switch>
+
+        <h1>Page test</h1>
       </header>
     </div>
   );
